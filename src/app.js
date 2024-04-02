c8c673e546ae72a0f2308d2d4a9442f4ad0732b8
wenxiyanF2023 <wenxiyan@andrew.cmu.edu>
2024-04-01 23:54:27 -0700

    mount work order router to app.js

--- src/app.js
+++ src/app.js
@@ -4,9 +4,12 @@ import { ApolloServer } from 'apollo-server-express';
 import { typeDefs } from './graphql/schema.js';
 import { resolvers } from './graphql/resolvers.js';
 import { connectProducer } from './kafka/producer.js';
+import workOrderRouter from './routes/workOrderRoute.js';
 
 dotenv.config();
 const app = express();
+
+app.use('/produce', workOrderRouter);
 
 const startServer = async () => {
   //databaseContext = await initializeDatabase();
@@ -17,7 +20,6 @@ const startServer = async () => {
     resolvers,
   });
   await apolloServer.start(); // Make sure to start Apollo Server before applying middleware
-  
   apolloServer.applyMiddleware({ app });
 };
 
